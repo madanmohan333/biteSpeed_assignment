9c1f3334b02b02dfa64a3f731342c49bd8fbc745
madanmohan333 <shivaclasses406@gmail.com>
2026-03-03 00:03:02 +0530

    logic created to find all related contacts

--- src/services/identify.service.js
+++ src/services/identify.service.js
@@ -31,7 +31,42 @@ exports.handleIdentify = async (email, phoneNumber) => {
     };
   }  
 
+  // get all linked contact id (include secondary chains)
+  const contactIds = new Set();
 
+  existingContacts.forEach((c) => {
+    contactIds.add(c.id);
+    if (c.linkedId) contactIds.add(c.linkedId);
+  });
+
+  const allRelatedContacts = await prisma.contact.findMany({
+    where: {
+      OR: [
+        { id: { in: Array.from(contactIds) } },
+        { linkedId: { in: Array.from(contactIds) } },
+      ],
+    },
+  });  
+
+  //find primary contact (oldest createdAt)
+  let primaryContact = allRelatedContacts
+    .filter((c) => c.linkPrecedence === "primary")
+    .sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt))[0];
+
+  //convert other primaries to secondary if need
+  const otherPrimaries = allRelatedContacts.filter(
+    (c) =>
+      c.linkPrecedence === "primary" && c.id !== primaryContact.id
+  );
+  for (const contact of otherPrimaries) {
+    await prisma.contact.update({
+      where: { id: contact.id },
+      data: {
+        linkPrecedence: "secondary",
+        linkedId: primaryContact.id,
+      },
+    });
+  }  
 
   return {
     contact: {
