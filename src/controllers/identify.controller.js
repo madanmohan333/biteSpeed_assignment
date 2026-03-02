d244bee772fe8af2e24e332dca36d823c793e971
madanmohan333 <shivaclasses406@gmail.com>
2026-03-03 00:17:35 +0530

    validation check for email and mob no

--- src/controllers/identify.controller.js
+++ src/controllers/identify.controller.js
@@ -3,12 +3,22 @@ const identifyService = require("../services/identify.service");
 exports.identifyContact = async (req, res) => {
   try {
     const { email, phoneNumber } = req.body;
+    if (!email && !phoneNumber) {
+      return res.status(400).json({
+        error: "Either email or phoneNumber must be provided",
+      });
+    }
+    if (email && !/^\S+@\S+\.\S+$/.test(email)) {
+      return res.status(400).json({
+        error: "Invalid email format",
+      });
+    }
 
     const result = await identifyService.handleIdentify(email, phoneNumber);
 
     return res.status(200).json(result);
   } catch (error) {
-    console.error(error);
+    console.error("Controller Error:", error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 };
